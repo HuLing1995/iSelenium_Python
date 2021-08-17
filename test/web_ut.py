1e27c17a8666f6c51ef756fafe3e68d96ec96f1e
HuLing1995 <89034462+HuLing1995@users.noreply.github.com>
2021-08-17 11:55:17 +0800

    Update web_ut.py

--- test/web_ut.py
+++ test/web_ut.py
@@ -35,7 +35,7 @@ class ISelenium(unittest.TestCase):
             print('使用无界面方式运行', config.get('driver', 'chrome_driver'))
             chrome_options.add_argument("--headless")
 
-        self.driver = webdriver.Chrome(executable_path=config.get('driver', 'chrome_driver'),
+        self.driver = webdriver.Chrome(executable_path="/var/iSelenium_Python/chromedriver",
                                        options=chrome_options)
 
     @allure.story('Test key word 今日头条')
