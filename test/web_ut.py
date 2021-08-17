03ff1dba39a03bfca45baae87249751e5db90d11
HuLing1995 <89034462+HuLing1995@users.noreply.github.com>
2021-08-17 11:18:54 +0800

    Update web_ut.py

--- test/web_ut.py
+++ test/web_ut.py
@@ -15,6 +15,7 @@ class ISelenium(unittest.TestCase):
     def get_config(self):
         config = configparser.ConfigParser()
         config.read(os.path.join(os.environ["HOME"], 'iselenium.ini'))
+        print(os.environ["HOME"])
         return config
 
     def tearDown(self):
